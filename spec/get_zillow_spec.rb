13ec47a7d28ef186153150a8bf85d69179fcf92c
James Banh <jbanh@ten-x.com>
2018-04-28 23:31:21 -0700

    changing id to global

--- spec/get_zillow_spec.rb
+++ spec/get_zillow_spec.rb
@@ -4,10 +4,9 @@ require 'nokogiri'
 require_relative '../lib/test_data'
 conn = Faraday.new(:url => 'http://www.zillow.com/webservice/')
 response = conn.get 'GetDeepSearchResults.htm',
-                    {:'zws-id' => 'X1-ZWz1gdr6wmx3bf_a80zd',
+                    {:'zws-id' => @zwsid,
                      :'address' => '12582-Cardinal-Avenue',
                      :'citystatezip' => '92843'}
-
 
 context 'when result returns' do
   it 'check status' do
